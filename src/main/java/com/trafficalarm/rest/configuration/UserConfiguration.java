e8d09839a5fe78d715b024f9f39126c3e164b1d6
mustafa yildirim <mustafayildirim001@gmail.com>
2015-04-03 12:28:22 +0300

    @EnableAspectJAutoProxy(proxyTargetClass=true) annotation is added into UserConfigruation class

--- src/main/java/com/trafficalarm/rest/configuration/UserConfiguration.java
+++ src/main/java/com/trafficalarm/rest/configuration/UserConfiguration.java
@@ -5,6 +5,7 @@ import javax.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
@@ -20,6 +21,7 @@ import com.trafficalarm.rest.mvc.AccountController;
 import com.trafficalarm.rest.mvc.MeResource;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class UserConfiguration {
     
     @Autowired
